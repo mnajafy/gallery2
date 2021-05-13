64d6655dd2a8735a486654b5b50a01ead07b4c2d
mnajafy <64306946+mnajafy@users.noreply.github.com>
2021-05-13 08:03:29 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,22 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    component: () => import(/* webpackChunkName: "services" */ '../views/Services.vue')
+  },
+  {
+    path: '/gallery',
+    name: 'Gallery',
+    component: () => import(/* webpackChunkName: "gallery" */ '../views/Gallery.vue')
   }
 ]
 
