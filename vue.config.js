49db670034be12ced9874205a99cca399ee337d9
mnajafy <64306946+mnajafy@users.noreply.github.com>
2021-05-13 08:05:40 +0200

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+
+// assetsPublicPath: '/',
+// assetsSubDirectory: 'static',
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+module.exports = {
+    chainWebpack: config => {
+        config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+            options.transformAssetUrls = {
+            img: 'src',
+            image: 'xlink:href',
+            'b-avatar': 'src',
+            'b-img': 'src',
+            'b-img-lazy': ['src', 'blank-src'],
+            'b-card': 'img-src',
+            'b-card-img': 'src',
+            'b-card-img-lazy': ['src', 'blank-src'],
+            'b-carousel-slide': 'img-src',
+            'b-embed': 'src'
+            }
+
+            return options
+        })
+    }
+}
