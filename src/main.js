8cde62dde8ca267fb249c836a5148e2a4d71e7e0
mnajafy <64306946+mnajafy@users.noreply.github.com>
2021-05-13 08:03:24 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faTwitter, faInstagram, faGithub, faLinkedinIn } from '@fortawesome/fontawesome-free-brands'
+import { faBars, faEnvelope, faPhone } from '@fortawesome/fontawesome-free-solid'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './assets/scss/app.scss'
 
+library.add(
+  faFacebook, 
+  faTwitter, 
+  faInstagram, 
+  faGithub, 
+  faBars, 
+  faLinkedinIn, 
+  faEnvelope, 
+  faPhone
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
