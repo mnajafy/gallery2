cafbd48e0ae90af4c10920335e204af03d141e2b
mnajafy <64306946+mnajafy@users.noreply.github.com>
2021-05-13 08:05:16 +0200

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,57 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <b-jumbotron>
+            <b-breadcrumb :items="items"></b-breadcrumb>
+        </b-jumbotron>
+
+        <b-container>
+            <section class="py-3 py-md-4 py-lg-5">
+                <b-row>
+                    <b-col cols="12" md="4" class="d-flex justify-content-center align-items-center">
+                        <div class="about-img">
+                            <b-img src="../assets/galleries/images/beta.jpg" fluid ></b-img> 
+                        </div>
+                    </b-col>
+
+                    <b-col cols="12" md="8">
+                        <b-card class="" title="hello world !" sub-title="Card subtitle">
+                            <b-card-text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos quibusdam dicta dolorem omnis ipsa tenetur nesciunt at possimus debitis ea!</b-card-text>
+                            <b-card-text>Dolorem omnis ipsa tenetur nesciunt at possimus debitis ea!</b-card-text>
+                            <ul>
+                                <li>Lorem ipsum dolor sit.</li>
+                                <li>Lorem ipsum dolor sit.</li>
+                                <li>Lorem ipsum dolor sit.</li>
+                            </ul>                            
+                            <b-card-text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos quibusdam dicta dolorem omnis ipsa tenetur nesciunt at possimus debitis ea!</b-card-text>
+                            <b-card-text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos quibusdam dicta dolorem omnis ipsa tenetur nesciunt at possimus debitis ea!</b-card-text>
+                        </b-card>
+                    </b-col>
+                </b-row>
+            </section>
+        </b-container>
+    </div>
 </template>
+
+<script>
+
+export default {
+    name: 'About',
+    components: {
+    //   Banner
+    },
+    data: () => {
+        return {
+            items: [
+                {
+                    text: 'Home',
+                    href: '#/'
+                },
+                {
+                    text: 'About',
+                    href: true
+                }
+            ]
+        }
+    }
+}
+</script>
